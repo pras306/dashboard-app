a015f17e306608821efb977315dbf8ccecd2455c
pras306 <44067654+pras306@users.noreply.github.com>
2022-05-17 17:07:02 +0530

    Initial Commit

---
+++ src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.css';
+
+const App = () => {
+  return(
+    <div className='app'>
+      <h1>Welcome to Dashboard Homepage</h1>
+    </div>
+  )
+}
+
+export default App;
